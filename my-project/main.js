e94239386336e932ddf1ac0107c69b92c7f8612d
MarcusY06 <90736630+MarcusY06@users.noreply.github.com>
2022-03-08 13:00:22 -0500

    a
    a

--- my-project/main.js
+++ my-project/main.js
@@ -1,1 +1,6 @@
 import "./index.css";
+import { gsap } from "gsap";
+
+const tl = gsap.timeline({ delay: 0.3 });
+tl.from(".header-title", { opacity: 0, duration: 0.3, ease: "ease-out" });
+tl.from(".navigation-bar", { opacity: 0, duration: 0.5, ease: "ease-out" });
