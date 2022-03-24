13315a878ea130f6c0aee0a90f251a587f50b979
MarcusY06 <90736630+MarcusY06@users.noreply.github.com>
2022-03-24 13:58:48 -0400

    a
    a

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,16 +1,29 @@
 module.exports = {
   content: ["./index.html", "./js/main.js", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  mode: "jit",
   theme: {
     extend: {},
   },
   plugins: [],
   theme: {
+    fontFamily: {
+      parisienne: "'Parisienne', serif",
+      hubballi: "'Hubballi', mono",
+      cormorant_garamond: "'Cormorant Garamond'",
+    },
     extend: {
-      fontFamily: {
-        parisienne: "'Parisienne', serif",
-        hubballi: "'Hubballi', mono",
-        cormorant_garamond: "'Cormorant Garamond'",
+      // that is animation class
+      animation: {
+        fade: "fadeOut 5s ease-in",
       },
+
+      // that is actual animation
+      keyframes: (theme) => ({
+        fadeOut: {
+          "0%": { backgroundColor: theme("colors.transparent") },
+          "100%": { backgroundColor: theme("colors.red.300") },
+        },
+      }),
     },
   },
 };
