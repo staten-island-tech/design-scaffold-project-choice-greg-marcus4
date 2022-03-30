9eccef91d63ecf8f026fc30bc1eb20f9d77ae554
MarcusY06 <90736630+MarcusY06@users.noreply.github.com>
2022-03-30 13:30:30 -0400

    a
    a

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -4,3 +4,19 @@ import { gsap } from "gsap";
 const tl = gsap.timeline({ delay: 0.3 });
 tl.from(".header-title", { opacity: 0, duration: 0.3, ease: "ease-out" });
 tl.from(".navigation-bar", { opacity: 0, duration: 0.5, ease: "ease-out" });
+
+const headers = gsap.utils.toArray(".header");
+
+headers.forEach((header, i) => {
+  const anim = gsap.fromTo(
+    header,
+    { autoAlpha: 0, y: 100 },
+    { duration: 1, autoAlpha: 1, y: 0 }
+  );
+  ScrollTrigger.create({
+    trigger: header,
+    animation: anim,
+    toggleActions: "play none none none",
+    once: true,
+  });
+});
